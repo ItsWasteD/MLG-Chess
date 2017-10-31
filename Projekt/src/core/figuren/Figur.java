14841b2b23bc5753459560c285481b5769cd44e9
David Roth <david.roth@iet-gibb.ch>
2017-10-31 09:24:26 +0100

    Added folder structure

---
+++ Projekt/src/core/figuren/Figur.java
@@ -0,0 +1,11 @@
+package core.figuren;
+
+public abstract class Figur {
+
+    private Feld feld;
+    private String farbe;
+    private boolean isDead;
+
+    private abstract boolean
+
+}
---
+++ Projekt/src/core/figuren/Moveable.java
@@ -0,0 +1,5 @@
+package core.figuren;
+
+public interface Moveable {
+    boolean moveTo(int x, int y);
+}
