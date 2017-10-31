54f131628b0a7418f350d4b2cf408e4d646f6a40
Yanis Noa Weibel <yanis.noa.weibel@iet-gibb.ch>
2017-10-31 11:23:54 +0100

    startet with figurLogik

--- Projekt/src/core/figuren/Pawn.java
+++ Projekt/src/core/figuren/Pawn.java
@@ -1,16 +1,32 @@
 package core.figuren;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class Pawn extends Figur{
+public class Pawn extends Figur {
+
+    private final List<int[][]> moves = new ArrayList<>();
+    private List<int[][]> possibleMoves;
 
     public Pawn(String color) {
+                moves.add(2, 0);
+                moves.add({-2}{0});
+                moves.add({1, 1});
+                moves.add({1, 0});
+                moves.add({1, -1});
+                moves.add({-1, 1});
+                moves.add({-1, 0});
+                moves.add({-1, -1});
         super(color);
     }
 
     @Override
     public ArrayList<int[][]> getPossibleMoves() {
-
+        if (isOnStartPosition() && color == black) {
+            possibleMoves
+        }
         return null;
     }
 
