9262aa087b4c36359cb3d27061eab9162ee65584
Yanis Noa Weibel <yanis.noa.weibel@iet-gibb.ch>
2017-10-31 10:10:29 +0100

    deleted Moveable interface FigurService + Controller implementiert

---
+++ Projekt/src/api/figur/FigurController.java
@@ -0,0 +1,12 @@
+package api.figur;
+
+import core.figuren.Figur;
+
+public class FigurController {
+
+    private void movefigur(Figur figur,int x,int y){
+
+    }
+
+
+}
--- Projekt/src/core/figuren/Figur.java
+++ Projekt/src/core/figuren/Figur.java
@@ -1,21 +1,14 @@
 package core.figuren;
 
-import api.Moveable;
 import game.Feld;
 
 import java.util.ArrayList;
 
-public abstract class Figur implements Moveable {
+public abstract class Figur {
 
     private Feld feld;
     private String farbe;
     private boolean isDead;
-
-    @Override
-    public boolean moveTo(int x, int y) {
-
-        return false;
-    }
 
     public abstract ArrayList<int[][]> getPossibleMoves();
 }
