1f9b9b1670c4fdbd682b2172ff3a24c6555a72ca
Yanis Noa Weibel <yanis.noa.weibel@iet-gibb.ch>
2017-11-28 08:54:44 +0100

    best GUI change ever

--- Projekt/src/gui/Main.java
+++ Projekt/src/gui/Main.java
@@ -1,11 +1,9 @@
 package gui;
 
-import api.database.Database;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,9 +15,13 @@ public class Main extends Application {
         launch(args);
     }
 
+    public static Stage getStage() {
+        return primaryStage;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        this.primaryStage = primaryStage;
+        Main.primaryStage = primaryStage;
 
         primaryStage.setTitle("MLG Chess");
 
@@ -28,9 +30,5 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    public static Stage getStage(){
-        return primaryStage;
     }
 }
