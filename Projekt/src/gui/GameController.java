306d5d5522802ab2b2c592e58aaa946b1b577fc8
Tim Leibacher <tim.jeremy.leibacher@iet-gibb.ch>
2017-12-19 10:32:37 +0100

    game

---
+++ Projekt/src/gui/GameController.java
@@ -0,0 +1,71 @@
+package gui;
+
+import core.java.figuren.Bishop;
+import core.java.figuren.Figur;
+import game.java.Chessfield;
+import game.java.Field;
+import gui.Main;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.awt.*;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameController implements Initializable {
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+       /* Chessfield cf = new Chessfield();
+        Field[][] chessfield = Chessfield.getChessfield();*/
+
+/*        File f = new File("Projekt\\src\\gui\\images\\face.jpg");
+
+        Image image = new Image(f.toURI().toString(),true);
+
+
+
+        ImageView imgView = new ImageView();
+        imgView.setY(10);
+        imgView.setX(10);
+        imgView.setVisible(true);
+
+        imgView.setImage(image);
+
+        System.out.println(imgView.getImage());
+
+        HBox hbox = new HBox(imgView);
+
+        Scene scene = new Scene(hbox, 200, 100);
+        Main.getStage().setScene(scene);
+        Main.getStage().show();*/
+
+
+/*        for(Field[] field : chessfield){
+
+            for (Field realField : field){
+                System.out.println(realField.ROW + " " + realField.COLUMN);
+            }
+
+        }*/
+
+
+        }
+        }
+
+
--- Projekt/src/gui/MainController.java
+++ Projekt/src/gui/MainController.java
@@ -1,40 +1,56 @@
 package gui;
 
-import api.game.GameController;
+import core.java.figuren.*;
+import game.java.Chessfield;
+import game.java.Field;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.effect.BlendMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 import javax.swing.*;
+import java.awt.font.ImageGraphicAttribute;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
 
     public ImageView imgView;
+    private boolean isYellow = false;
 
     public MainController() {
 
     }
 
     public void quit(ActionEvent actionEvent) {
-       Main.getStage().close();
+        Main.getStage().close();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         File f = new File("Projekt\\src\\gui\\images\\face.jpg");
+        new Chessfield();
 
-        Image image = new Image(f.toURI().toString(),true);
+        Image image = new Image(f.toURI().toString(), true);
         imgView.setImage(image);
     }
 
@@ -54,17 +70,205 @@ public class MainController implements Initializable {
         Main.getStage().setScene(scene);
     }
 
-    public void startGame(ActionEvent actionEvent) throws IOException {
+    public void startGame() throws IOException {
 
-        Parent root = FXMLLoader.load(getClass().getResource("Game.fxml"));
+       /*Parent root = FXMLLoader.load(getClass().getResource("Game.fxml"));
 
         Scene scene = new Scene(root);
 
+        Main.getStage().setScene(scene);*/
+
+
+
+
+        Pane canvas = new Pane();
+        canvas.setStyle("-fx-background-color: white;");
+
+
+        File fileBlack = new File("Projekt\\src\\gui\\images\\black.jpg");
+        File fileWhite = new File("Projekt\\src\\gui\\images\\white.jpg");
+        File fileYellow = new File("Projekt\\src\\gui\\images\\yellow.jpg");
+
+        Image imageBlack = new Image(fileBlack.toURI().toString(), true);
+        Image imageWhite = new Image(fileWhite.toURI().toString(), true);
+        Image imageYellow = new Image(fileYellow.toURI().toString(), true);
+
+
+        ImageView imgView2 = new ImageView();
+        imgView2.setImage(imageWhite);
+
+        VBox vBox = new VBox();
+
+
+        for (Field[] field : Chessfield.getChessfield()) {
+            HBox hbox = new HBox();
+            for (Field realField : field) {
+                ImageView imgView = new ImageView();
+
+                if (realField.getFigur() != null) {
+                    imgView.setImage(getImage(realField.getFigur()));
+                    imgView.setId(realField.ROW + "_" + realField.COLUMN);
+                    imgView.setOnMouseClicked(event -> {
+                        if (!isYellow){
+
+                        ArrayList<Field> poissibleMoves = realField.getFigur().getPossibleMoves();
+
+                        for (Field f : poissibleMoves) {
+                            for (Node child : vBox.getChildren()) {
+                                HBox tempHBox = (HBox) child;
+                                for (Node iv : tempHBox.getChildren()) {
+                                    if (iv instanceof ImageView) {
+                                        ImageView tempIv = (ImageView) iv;
+                                        String possibleField = f.ROW + "_" + f.COLUMN;
+                                        if (tempIv.getId().equals(possibleField)) {
+                                            tempIv.setImage(imageYellow);
+                                            isYellow = true;
+                                        }
+                                    }
+                                }
+                            }
+                            //ImageView img = vBox.getChildren()
+                        }
+
+                    }else{
+                            try {
+                                isYellow = false;
+                                startGame();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }});
+
+                    ImageView white = new ImageView();
+                    white.setImage(imageWhite);
+                    ImageView black = new ImageView();
+                    black.setImage(imageBlack);
+
+                    Group g = null;
+
+
+                    if ((realField.ROW + realField.COLUMN) % 2 == 0) {
+                        g = new Group(white, imgView);
+                    } else {
+                        g = new Group(black, imgView);
+                    }
+
+
+                    hbox.getChildren().add(g);
+                }
+
+                //Is white field
+                else if ((realField.ROW + realField.COLUMN) % 2 == 0) {
+                    imgView.setImage(imageWhite);
+                    imgView.setId(realField.ROW + "_" + realField.COLUMN);
+                    imgView.setOnMouseClicked(event -> {
+                        System.out.println(imgView.getId());
+
+                    });
+                    hbox.getChildren().add(imgView);
+                } else {
+                    imgView.setImage(imageBlack);
+                    hbox.getChildren().add(imgView);
+                    imgView.setId(realField.ROW + "_" + realField.COLUMN);
+                    imgView.setOnMouseClicked(event -> {
+                        System.out.println(imgView.getId());
+                    });
+                }
+
+            }
+            vBox.getChildren().add(hbox);
+            loadFigures();
+        }
+
+        canvas.getChildren().add(vBox);
+
+        Scene scene = new Scene(canvas, 400, 400);
+
+
         Main.getStage().setScene(scene);
+        Main.getStage().show();
+    }
+
+    public void loadFigures() {
+
     }
 
     public void doStuff(MouseEvent mouseEvent) {
 
         JOptionPane.showMessageDialog(null, "Such WOW. You cool", "MLG", JOptionPane.OK_CANCEL_OPTION);
+    }
+
+    public Image getImage(Figur figur) {
+
+
+        File file_bishop_black = new File("Projekt\\src\\gui\\images\\bishop.png");
+        File file_knight_black = new File("Projekt\\src\\gui\\images\\knight.png");
+        File file_pawn__black = new File("Projekt\\src\\gui\\images\\pawn.png");
+        File file_queen_black = new File("Projekt\\src\\gui\\images\\queen.png");
+        File file_rook_black = new File("Projekt\\src\\gui\\images\\rook.png");
+        File file_king_black = new File("Projekt\\src\\gui\\images\\king.png");
+
+        File file_bishop_white = new File("Projekt\\src\\gui\\images\\w_bishop.png");
+        File file_knight_white = new File("Projekt\\src\\gui\\images\\w_knight.png");
+        File file_pawn__white = new File("Projekt\\src\\gui\\images\\w_pawn.png");
+        File file_queen_white = new File("Projekt\\src\\gui\\images\\w_queen.png");
+        File file_rook_white = new File("Projekt\\src\\gui\\images\\w_rook.png");
+        File file_king_white = new File("Projekt\\src\\gui\\images\\w_king.png");
+
+        Image image_bishop_black = new Image(file_bishop_black.toURI().toString(), true);
+        Image image_knight_black = new Image(file_knight_black.toURI().toString(), true);
+        Image image_pawn_black = new Image(file_pawn__black.toURI().toString(), true);
+        Image image_queen_black = new Image(file_queen_black.toURI().toString(), true);
+        Image image_rook_black = new Image(file_rook_black.toURI().toString(), true);
+        Image image_king_black = new Image(file_king_black.toURI().toString(), true);
+
+        Image image_bishop_white = new Image(file_bishop_white.toURI().toString(), true);
+        Image image_knight_white = new Image(file_knight_white.toURI().toString(), true);
+        Image image_pawn_white = new Image(file_pawn__white.toURI().toString(), true);
+        Image image_queen_white = new Image(file_queen_white.toURI().toString(), true);
+        Image image_rook_white = new Image(file_rook_white.toURI().toString(), true);
+        Image image_king_white = new Image(file_king_white.toURI().toString(), true);
+
+        if (figur.isWhite()) {
+            if (figur instanceof Pawn) {
+                return image_pawn_white;
+            }
+            if (figur instanceof Knight) {
+                return image_knight_white;
+            }
+            if (figur instanceof Queen) {
+                return image_queen_white;
+            }
+            if (figur instanceof Rook) {
+                return image_rook_white;
+            }
+            if (figur instanceof King) {
+                return image_king_white;
+            }
+            if (figur instanceof Bishop) {
+                return image_bishop_white;
+            }
+        } else {
+            if (figur instanceof Pawn) {
+                return image_pawn_black;
+            }
+            if (figur instanceof Knight) {
+                return image_knight_black;
+            }
+            if (figur instanceof Queen) {
+                return image_queen_black;
+            }
+            if (figur instanceof Rook) {
+                return image_rook_black;
+            }
+            if (figur instanceof King) {
+                return image_king_black;
+            }
+            if (figur instanceof Bishop) {
+                return image_bishop_black;
+            }
+        }
+
+        return image_bishop_white;
     }
 }
